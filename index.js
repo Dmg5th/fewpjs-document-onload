111881fe3c91a47d3517dd1a0a3aad7f18327abb
Dmg5th <dmgould310@gmail.com>
2020-12-30 15:14:19 -0500

    commit tree fd85297790dd492e4a19271c7e0c37757a3b9b15

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let target = document.getElementById("text")
+    target.innerHTML = "This is really cool!"
+})
